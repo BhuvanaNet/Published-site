459ac8269de8526db1703d84ec5eb494344052bb
BhuvanaNet <sabhuso10@gmail.com>
2017-02-08 12:25:30 +0000

    Webrole complete

---
+++ GuestBook_Data/GuestBookDataSource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure;
+
+
+namespace GuestBook_Data
+{
+    public class GuestBookDataSource
+    {
+        private static CloudStorageAccount storageAccount;
+        private GuestBookDataContext context;
+        static GuestBookDataSource()
+        {
+            storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+                
+
+            CloudTableClient cloudTableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = cloudTableClient.GetTableReference("GuestBookEntity");
+            table.CreateIfNotExists();
+        }
+
+        public GuestBookDataSource()
+        {
+            this.context = new GuestBookDataContext(storageAccount.CreateCloudTableClient());
+        }
+
+        public IEnumerable<GuestBookEntity> GetGuestBookEntries()
+        {
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("GuestBookEntity");
+
+            TableQuery<GuestBookEntity> query = new TableQuery<GuestBookEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, DateTime.UtcNow.ToString("MMddyyyy")));
+
+            return table.ExecuteQuery(query);
+        }
+
+        public void AddGuestBookEntity(GuestBookEntity newItem)
+        {
+            TableOperation operation = TableOperation.Insert(newItem);
+            CloudTable table = context.ServiceClient.GetTableReference("GuestBookEntity");
+            table.Execute(operation);
+        }
+
+        public void UpdateImageThumbnail(string partitionKey, string rowKey, string thumbUrl)
+        {
+            CloudTable table = context.ServiceClient.GetTableReference("GuestBookEntity");
+            TableOperation retrieveOperation = TableOperation.Retrieve<GuestBookEntity>(partitionKey, rowKey);
+
+            TableResult retrieveResult = table.Execute(retrieveOperation);
+            GuestBookEntity updateEntity = (GuestBookEntity)retrieveResult.Result;
+
+            if (updateEntity != null)
+            {
+                updateEntity.ThumbnailUrl = thumbUrl;
+
+                TableOperation replaceOperation = TableOperation.Replace(updateEntity);
+                table.Execute(replaceOperation);
+            }
+        }
+    }
+}
--- GuestBook_WebRole/Default.aspx.cs
+++ GuestBook_WebRole/Default.aspx.cs
@@ -4,14 +4,107 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using GuestBook_Data;
+using System.IO;
+using Microsoft.Azure;
+using System.Net;
 
 namespace GuestBook_WebRole
 {
-    public partial class _Default : Page
+    public partial class _Default : System.Web.UI.Page
     {
+        private static bool storageInitialized = false;
+        private static object gate = new object();
+        private static CloudBlobClient blobStorage;
+        private static CloudQueueClient queueStorage;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                this.Timer1.Enabled = true;
+            }
+        }
 
+        protected void SignButton_Click(object sender, EventArgs e)
+        {
+            if (this.FileUpload1.HasFile)
+            {
+                this.InitializeStorage();
+
+                string uniqueBlobName = string.Format("guestbookpics/image_{0}{1}", Guid.NewGuid().ToString(), Path.GetExtension(this.FileUpload1.FileName));
+                CloudBlockBlob blob = blobStorage.GetContainerReference("guestbookpics").GetBlockBlobReference(uniqueBlobName);
+                blob.Properties.ContentType = this.FileUpload1.PostedFile.ContentType;
+                blob.UploadFromStream(this.FileUpload1.FileContent);
+
+                GuestBookEntity entry = new GuestBookEntity()
+                {
+                    GuestName = this.NameTextBox.Text,
+                    Message = this.MessageTextBox.Text,
+                    PhotoUrl = blob.Uri.ToString(),
+                    ThumbnailUrl = blob.Uri.ToString()
+                };
+                GuestBookDataSource ds = new GuestBookDataSource();
+                ds.AddGuestBookEntity(entry);
+
+                var queue = queueStorage.GetQueueReference("guestthumbs");
+                var message = new CloudQueueMessage(string.Format("{0},{1},{2}", uniqueBlobName, entry.PartitionKey, entry.RowKey));
+                queue.AddMessage(message);
+
+
+
+            }
+
+            this.NameTextBox.Text = string.Empty;
+            this.MessageTextBox.Text = string.Empty;
+            this.DataList1.DataBind();
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            this.DataList1.DataBind();
+        }
+
+        private void InitializeStorage()
+        {
+            if (storageInitialized)
+            {
+                return;
+            }
+            lock (gate)
+                if (storageInitialized)
+                {
+                    return;
+                }
+            try
+            {
+
+                var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+
+                blobStorage = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobStorage.GetContainerReference("guestbookpics");
+                container.CreateIfNotExists();
+
+                var permissions = container.GetPermissions();
+                permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                container.SetPermissions(permissions);
+
+                queueStorage = storageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueStorage.GetQueueReference("guestthumbs");
+                queue.CreateIfNotExists();
+
+            }
+            catch (WebException)
+            {
+                throw new WebException("skdfjslkjd");
+
+            }
+            storageInitialized = true;
         }
     }
 }
